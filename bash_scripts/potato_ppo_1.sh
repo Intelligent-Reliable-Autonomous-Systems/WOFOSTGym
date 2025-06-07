9f816ea19baf1b63b200a4154ad06998215bcd43
William Solow <soloww@submit-a.ib.coehpc>
2025-06-07 12:00:11 -0700

    experiments sync

--- bash_scripts/potato_ppo_1.sh
+++ bash_scripts/potato_ppo_1.sh
@@ -3,7 +3,7 @@
 #SBATCH -p eecs,gpu,share
 #SBATCH -o output/potato_ppo_1.out
 #SBATCH -e output/potato_ppo_1.err
-#SBATCH -t 3-00:00:00
+#SBATCH -t 6-00:00:00
 #SBATCH --gres=gpu:1
 
 python3 train_agent.py --agent-type PPO  --PPO.total-timesteps 10000000  --npk.output-vars "[FIN, WSO, DVS, NAVAIL, PAVAIL, KAVAIL, SM, TOTN, TOTP, TOTK, TOTIRRIG]" --npk.weather-vars "[IRRAD, TEMP, RAIN]" --agro-file potato_agro.yaml --npk.random-reset --npk.domain-rand --npk.scale 0.03  --npk.intvn-interval 7 --save-folder experiments/PartialObsConstrainedControl/Potato/ --env-reward RewardLimitedRunoffWrapper

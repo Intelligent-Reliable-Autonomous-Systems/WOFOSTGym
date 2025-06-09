f0600b7feb32f237c19d4b43366be77e3739933d
Will Solow <soloww@oregonstate.edu>
2025-06-09 11:54:34 -0700

    grape experiments

---
+++ bash_scripts/grape_ppo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -J grape_ppo
+#SBATCH -p eecs,gpu,share
+#SBATCH -o output/grape_ppo.out
+#SBATCH -e output/grape_ppo.err
+#SBATCH -t 6-00:00:00
+#SBATCH --gres=gpu:1
+
+python3 train_agent.py --agent-type PPO --agro-file grape.yaml --env-id grape-lnpkw-v0 --PPO.total-timesteps 5000000  --npk.random-reset --npk.domain-rand --npk.scale 0.03  --save-folder experiments/UncontrainedControl/Grape/
---
+++ bash_scripts/grape_sac.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -J grape_sac
+#SBATCH -p eecs,gpu,share
+#SBATCH -o output/grape_sac.out
+#SBATCH -e output/grape_sac.err
+#SBATCH -t 6-00:00:00
+#SBATCH --gres=gpu:1
+
+python3 train_agent.py --agent-type SAC --agro-file grape.yaml --env-id grape-lnpkw-v0 --SAC.total-timesteps 5000000  --npk.random-reset --npk.domain-rand --npk.scale 0.03  --save-folder experiments/UncontrainedControl/Grape/

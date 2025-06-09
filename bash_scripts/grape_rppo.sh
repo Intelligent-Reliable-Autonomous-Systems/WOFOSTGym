8bc71d118f49aefaa747b5ea4edb56e755060095
William Solow <soloww@submit-a.ib.coehpc>
2025-06-09 11:58:14 -0700

    experiments fix

--- bash_scripts/grape_rppo.sh
+++ bash_scripts/grape_rppo.sh
@@ -3,7 +3,7 @@
 #SBATCH -p eecs,gpu,share
 #SBATCH -o output/grape_rppo.out
 #SBATCH -e output/grape_rppo.err
-#SBATCH -t 6-00:00:00
+#SBATCH -t 4-12:00:00
 #SBATCH --gres=gpu:1
 
 python3 train_agent.py --agent-type DQN --agro-file grape.yaml --env-id grape-lnpkw-v0 --RPPO.total-timesteps 5000000  --npk.random-reset --npk.domain-rand --npk.scale 0.03  --save-folder experiments/UncontrainedControl/Grape/
